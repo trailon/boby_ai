d0b363bb78de190ac264e3fd9a659aeb7ee14805
trailon <cetinelv@gmail.com>
2026-01-05 00:42:25 +0300

    Update AppRouter to include OnboardingFavoritesRoute and adjust route naming convention

--- lib/app/router/router.dart
+++ lib/app/router/router.dart
@@ -1,14 +1,15 @@
 import 'dart:io';
 
 import 'package:auto_route/auto_route.dart';
+import 'package:boby_ai/app/router/router.gr.dart';
 
-@AutoRouterConfig(replaceInRouteName: 'View,Route')
+@AutoRouterConfig(replaceInRouteName: 'Screen,Route')
 class AppRouter extends RootStackRouter {
   @override
   RouteType get defaultRouteType => Platform.isAndroid ? const RouteType.material() : const RouteType.cupertino();
 
   @override
-  List<AutoRoute> get routes => [];
+  List<AutoRoute> get routes => [AutoRoute(page: OnboardingFavoritesRoute.page, initial: true)];
 }
 
 final appRouter = AppRouter();
--- lib/app/router/router.gr.dart
+++ lib/app/router/router.gr.dart
@@ -7,3 +7,24 @@
 
 // ignore_for_file: type=lint
 // coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:auto_route/auto_route.dart' as _i2;
+import 'package:boby_ai/presentation/onboarding/favorites/onboarding_favorites_screen.dart'
+    as _i1;
+
+/// generated route for
+/// [_i1.OnboardingFavoritesScreen]
+class OnboardingFavoritesRoute extends _i2.PageRouteInfo<void> {
+  const OnboardingFavoritesRoute({List<_i2.PageRouteInfo>? children})
+    : super(OnboardingFavoritesRoute.name, initialChildren: children);
+
+  static const String name = 'OnboardingFavoritesRoute';
+
+  static _i2.PageInfo page = _i2.PageInfo(
+    name,
+    builder: (data) {
+      return const _i1.OnboardingFavoritesScreen();
+    },
+  );
+}
