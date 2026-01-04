01443ec4dc73cf65e8f58c962b4c1bab5ae6cef7
trailon <cetinelv@gmail.com>
2026-01-05 00:33:33 +0300

    Integrate app theme into MaterialApp for consistent dark mode styling

--- lib/boby_ai.dart
+++ lib/boby_ai.dart
@@ -1,6 +1,7 @@
 import 'package:boby_ai/app/env/app_flavor.dart';
 import 'package:boby_ai/app/localization/generated/l10n.dart';
 import 'package:boby_ai/app/router/router.dart';
+import 'package:boby_ai/app/theme/app_theme.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -13,6 +14,8 @@ class BobyAi extends StatelessWidget {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.dark,
+      theme: buildAppTheme(),
+      darkTheme: buildAppTheme(),
       builder: (context, child) {
         debugPrint("TextScale: ${MediaQuery.of(context).textScaler.toString()}");
         if (!kReleaseMode) {
