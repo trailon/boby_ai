50f9db0e6dc4eb724feec8f3e3ce697f7c199ac6
trailon <cetinelv@gmail.com>
2026-01-04 16:27:04 +0300

    Add BobyAi widget and update main function to initialize app with new structure

---
+++ lib/boby_ai.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BobyAi extends StatelessWidget {
+  const BobyAi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
