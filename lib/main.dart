30f31436e78501445a1d1a5d5f36610fe56a6cbe
trailon <cetinelv@gmail.com>
2026-01-04 18:04:07 +0300

    Initialize Flutter Native Splash to preserve the splash screen during app startup

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:boby_ai/app/env/app_flavor.dart';
 import 'package:boby_ai/app/env/envied.dart';
 import 'package:boby_ai/boby_ai.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   flavor = const String.fromEnvironment('flavor').flavor;
   appEnv = AppEnv();
   runApp(const BobyAi());
