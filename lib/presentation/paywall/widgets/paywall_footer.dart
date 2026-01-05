f1fdfd4ed685624bb3f946961b434fee22412bb1
trailon <cetinelv@gmail.com>
2026-01-06 02:25:20 +0300

    Add _PaywallFooter widget to encapsulate footer elements for the paywall screen, improving layout organization and user interaction.

---
+++ lib/presentation/paywall/widgets/paywall_footer.dart
@@ -0,0 +1,19 @@
+part of '../paywall_screen.dart';
+
+class _PaywallFooter extends StatelessWidget {
+  final PaywallStore store;
+
+  const _PaywallFooter({required this.store});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _AutoRenewalText(),
+        SizedBox(height: 4.h),
+        _PaywallCTAButton(store: store),
+        _PaywallLegalLinks(),
+      ],
+    );
+  }
+}
