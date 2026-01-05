06d6b15f1da6f907788a0074ff19bf889f070fbe
trailon <cetinelv@gmail.com>
2026-01-06 02:25:25 +0300

    Add _PaywallContent widget to structure the paywall screen layout, integrating header, feature table, trial switcher, plan list, and footer for a cohesive user experience.

---
+++ lib/presentation/paywall/widgets/paywall_content.dart
@@ -0,0 +1,27 @@
+part of '../paywall_screen.dart';
+
+class _PaywallContent extends StatelessWidget {
+  final PaywallStore store;
+
+  const _PaywallContent({required this.store});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(24.w, 0.h, 24.w, 0.h),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _PaywallHeader(onClosePressed: () {}),
+          SizedBox(height: 16.h),
+          _PaywallFeatureTable(store: store),
+          SizedBox(height: 28.h),
+          _FreeTrialSwitcher(store: store),
+          SizedBox(height: 24.h),
+          _PlanList(store: store),
+          _PaywallFooter(store: store),
+        ],
+      ),
+    );
+  }
+}
