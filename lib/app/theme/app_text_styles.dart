ebc3151fc89fd412d69c1060eb37739caeafd4e6
trailon <cetinelv@gmail.com>
2026-01-06 02:23:35 +0300

    Refactor AppTextStyles to use ScreenUtil for responsive font sizes and add new text styles for improved typography management.

--- lib/app/theme/app_text_styles.dart
+++ lib/app/theme/app_text_styles.dart
@@ -1,35 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'app_colors.dart';
 
 abstract class AppTextStyles {
-  static const headlineMedium = TextStyle(
+  static TextStyle headlineMedium = TextStyle(
     fontFamily: 'Inter',
-    fontSize: 24,
+    fontSize: 24.sp,
     fontWeight: FontWeight.w700,
     color: AppColors.textPrimary,
   );
 
-  static const headlineSmall = TextStyle(
+  static TextStyle headlineSmall = TextStyle(
     fontFamily: 'Inter',
-    fontSize: 20,
+    fontSize: 20.sp,
     fontWeight: FontWeight.w500,
     color: AppColors.textPrimary,
   );
 
-  static const titleMedium = TextStyle(
+  static TextStyle titleMedium = TextStyle(
     fontFamily: 'Inter',
-    fontSize: 20,
+    fontSize: 20.sp,
     fontWeight: FontWeight.w600,
     color: AppColors.textPrimary,
   );
 
-  static const body = TextStyle(
+  static TextStyle titleSmall = TextStyle(
     fontFamily: 'Inter',
-    fontSize: 16,
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle labelMedium = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle bodyLarge = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 16.sp,
     fontWeight: FontWeight.w400,
     color: AppColors.textSecondary,
   );
 
-  static const button = TextStyle(fontFamily: 'Inter', fontSize: 16, fontWeight: FontWeight.w600, color: Colors.white);
+  static TextStyle bodyMedium = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w400,
+    color: AppColors.textSecondary,
+  );
+
+  static TextStyle bodySmall = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w400,
+    color: AppColors.textSecondary,
+  );
+
+  static TextStyle button = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w600,
+    color: Colors.white,
+  );
 }
