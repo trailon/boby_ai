a49f5745662e007a31c524300c02787cf2d738c4
trailon <cetinelv@gmail.com>
2026-01-06 02:23:30 +0300

    Add new color constants to AppColors for background fill, basic colors, and additional states to enhance theming capabilities.

--- lib/app/theme/app_colors.dart
+++ lib/app/theme/app_colors.dart
@@ -4,6 +4,7 @@ abstract class AppColors {
   // Backgrounds
   static const background = Color(0xFF0F0E0E);
   static const surface = Color(0xFF1A1919);
+  static const backgroundFillGray = Color(0x29787880);
 
   // Text
   static const textPrimary = Color(0xFFF3E9E9);
@@ -16,4 +17,11 @@ abstract class AppColors {
 
   // States
   static const disabled = Color(0xFF968D8D);
+  static const green = Color(0xFF00E275);
+  static const gray = Color(0xFFDED5D5);
+
+  // Basic Colors
+  static const white = Colors.white;
+  static const black = Colors.black;
+  static const grey = Colors.grey;
 }
