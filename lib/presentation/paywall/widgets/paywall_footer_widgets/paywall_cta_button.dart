ff3ca6d90d698bf81c37be72ce53610aaa56096f
trailon <cetinelv@gmail.com>
2026-01-06 06:25:46 +0300

    Implement dynamic text and animation for Paywall CTA button based on free trial status

--- lib/presentation/paywall/widgets/paywall_footer_widgets/paywall_cta_button.dart
+++ lib/presentation/paywall/widgets/paywall_footer_widgets/paywall_cta_button.dart
@@ -7,6 +7,26 @@ class _PaywallCTAButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppButton(text: S.current.paywall_footer_cta_button_state_price, onPressed: () {});
+    return Observer(
+      builder: (context) {
+        final String text = store.freeTrialEnabled
+            ? "${S.current.paywall_footer_cta_button_state_free_1}\n${S.current.paywall_footer_cta_button_state_free_2}"
+            : S.current.paywall_footer_cta_button_state_price;
+        Widget button = AppButton(text: text, onPressed: () {});
+        if (store.freeTrialEnabled) {
+          button = button
+              .animate(
+                autoPlay: true,
+                onComplete: (controller) {
+                  controller.repeat();
+                },
+              )
+              .scaleX(begin: 1, end: 1.1)
+              .then()
+              .scaleX(begin: 1.1, end: 1);
+        }
+        return button;
+      },
+    );
   }
 }
